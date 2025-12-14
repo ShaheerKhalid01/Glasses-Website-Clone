1014c6d77047cc92a38387a3a99ee784512d7356
ShaheerKhalid01 <www.shaheerkhalid88600@gmail.com>
2025-12-14 14:01:56 +0500

    Enable dark mode and update content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
@@ -16,7 +16,7 @@ module.exports = {
           400: '#38bdf8',
           500: '#0ea5e9',
           600: '#0284c7',
-          700: '#0369a1', // Add this line
+          700: '#0369a1',
           800: '#075985',
           900: '#0c4a6e',
         },
