3ebd6b91318f43dcb478434accf991e9e024bba8
ShaheerKhalid01 <www.shaheerkhalid88600@gmail.com>
2025-12-14 14:07:06 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,17 +1,17 @@
 'use client'
 import dynamic from 'next/dynamic'
-import Header from '../components/Header'
-import Hero from '../components/Hero'
+import Header from './components/Header'
+import Hero from './components/Hero'
 
 // Optimizing performance: Lazy load below-the-fold components
 // Imports fixed to point to root 'components' folder relative to 'app/page.js'
-const Features = dynamic(() => import('../components/Features'))
-const Categories = dynamic(() => import('../components/Categories'))
-const Products = dynamic(() => import('../components/Products'))
-const Footer = dynamic(() => import('../components/Footer'))
+const Features = dynamic(() => import('./components/Features'))
+const Categories = dynamic(() => import('./components/Categories'))
+const Products = dynamic(() => import('./components/Products'))
+const Footer = dynamic(() => import('./components/Footer'))
 
 // Load VR Button only on client side to avoid hydration issues and speed up initial HTML
-const VRFloatingButton = dynamic(() => import('../components/VRFloatingButton'), { ssr: false })
+const VRFloatingButton = dynamic(() => import('./components/VRFloatingButton'), { ssr: false })
 
 export default function Home() {
   return (
